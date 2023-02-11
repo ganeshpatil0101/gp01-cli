6272515f23977df456b877f1e5022b689559d1dc
Ganesh Patil <ganeshpatil0101@gmail.com>
2023-02-11 19:56:11 +0530

    new version released

--- bin/cli.js
+++ bin/cli.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 const argv = process.argv;
-console.log('gp01-cli@1.0.1');
-console.info('New Version Released')
+console.log('===================================');
+console.log('gp01-cli@1.0.2');
+console.info('New Version Released');
 console.log('args' , argv);
 
